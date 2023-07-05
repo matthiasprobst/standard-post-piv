1f699f5cdab49b8b1e84598aca1733c387510518
Matthias Probst <matth.probst@gmail.com>
2023-07-05 09:51:56 +0200

    introduced package config

--- standardpostpiv/__init__.py
+++ standardpostpiv/__init__.py
@@ -1,5 +1,6 @@
 import xarray as xr
 
+from standardpostpiv._cfg import set_config, get_config
 from . import _logging
 from . import plotting
 from . import standardplots
@@ -11,4 +12,5 @@ from .statistics import stats
 xr.set_options(keep_attrs=True)
 core_logger = _logging.create_package_logger('standardpostpiv')
 _logging.set_loglevel(core_logger, 'DEBUG')
-__all__ = ['__version__']
+
+__all__ = ['__version__', 'set_config', 'get_config', ]
